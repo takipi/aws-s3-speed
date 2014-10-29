870fcd54e90ca1c990d56bcb41296aa6e82b67bc
Markus Fix <lispmeister@gmail.com>
2014-10-29 14:32:12 +0400

    Added run.sh and setup.sh convenience shell scripts

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+JAR='aws-s3-speed-0.0.5-SNAPSHOT-jar-with-dependencies.jar'
+JAVA='/usr/bin/java'
+
+AWS_KEY='AKIAIMBGWFS6Z4NVITMQ'
+AWS_SECRET='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'
+
+PREFIX='aws-speed-test-'
+SUFFIX='-28-10-2014'
+
+ROUNDS='12'
+SIZE='BIG'
+METHOD='SDK'
+
+# run the speed test
+echo $JAVA -jar $JAR RUN $AWS_KEY $AWS_SECRET $PREFIX $SUFFIX $ROUNDS $SIZE $METHOD
+$JAVA -jar $JAR RUN $AWS_KEY $AWS_SECRET $PREFIX $SUFFIX $ROUNDS $SIZE $METHOD
+
